08dd096ac9c3c18e19009ebf419f10f3b928d571
Step 25 <bhuvanadeepika017@gmail.com>
2020-01-27 14:54:16 +0530

    added saving comments and displaying comments

--- app.js
+++ app.js
@@ -1,7 +1,48 @@
 const fs = require('fs');
 const Response = require('./lib/response');
 const CONTENT_TYPES = require('./lib/mimeTypes');
+const { loadTemplate } = require('./lib/viewTemplate');
 const STATIC_FOLDER = `${__dirname}/public`;
+
+const checkFileExist = function(filePath) {
+  let comments = [];
+  if (fs.existsSync(filePath)) comments = JSON.parse(fs.readFileSync(filePath));
+  return comments;
+}
+
+const generateComments = () => {
+  const dataFilePath = `${STATIC_FOLDER}/data/comments.json`;
+  const comments = checkFileExist(dataFilePath);
+  const generateComment = function(commentsHtml, comment) {
+    const html = `<tbody><td>${comment.date}</td>
+      <td>${comment.name}</td>
+      <td>${comment.comment}</td></tbody>`;
+    return html + commentsHtml;
+  };
+  const html = comments.reduce(generateComment, '');
+  return html;
+};
+
+const serveGuestBookPage = function() {
+  const commentsHtml = generateComments();
+  const html = loadTemplate('guestBook.html', { COMMENTS: commentsHtml });
+  const res = new Response();
+  res.setHeader('Content-Type', CONTENT_TYPES.html);
+  res.setHeader('Content-Length', html.length);
+  res.statusCode = 200;
+  res.body = html;
+  return res;
+}
+
+const saveCommentAndRedirect = function(req) {
+  const dataFilePath = `${STATIC_FOLDER}/data/comments.json`
+  const comments = checkFileExist(dataFilePath);
+  const date = new Date().toGMTString();
+  const { name, comment } = req.body;
+  comments.push({ date, name, comment });
+  fs.writeFileSync(dataFilePath, JSON.stringify(comments), 'utf8');
+  return serveGuestBookPage();
+}
 
 const serveStaticFile = (req, optionalUrl) => {
   const path = `${STATIC_FOLDER}${optionalUrl || req.url}`;
@@ -24,6 +65,8 @@ const serveHomePage = function(req) {
 
 const findHandler = (req) => {
   if (req.method === 'GET' && req.url === '/') return serveHomePage;
+  if (req.method === 'POST' && req.url === '/saveComment') return saveCommentAndRedirect;
+  if (req.method === 'GET' && req.url === '/guestBook.html') return serveGuestBookPage;
   if (req.method === 'GET') return serveStaticFile;
   return () => new Response();
 }
