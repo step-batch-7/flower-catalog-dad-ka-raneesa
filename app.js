0ce8709606cac257085ed3f2761f6af849eee3ac
Step 25 <bhuvanadeepika017@gmail.com>
2020-01-28 18:07:25 +0530

    modified server from nte module to http module

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const Response = require('./lib/response');
 const CONTENT_TYPES = require('./lib/mimeTypes');
 const { loadTemplate } = require('./lib/viewTemplate');
 const SYMBOLS = require('./lib/symbols');
@@ -15,7 +14,7 @@ const loadComments = function() {
 
 const generateComment = function(commentsHtml, commentDetails) {
   const { date, name, comment } = commentDetails;
-  const html = `<tbody><td class="date">${date}</td>
+  const html = `<tbody><td class="date">${new Date(date).toGMTString()}</td>
     <td class="name">${name}</td>
     <td class="comment">${comment.replace(/\n/g, '</br>')}</td></tbody>`;
   return html + commentsHtml;
@@ -27,15 +26,13 @@ const generateComments = () => {
   return html;
 };
 
-const serveGuestBookPage = function() {
+const serveGuestBookPage = function(req, res) {
   const commentsHtml = generateComments();
   const html = loadTemplate('guestBook.html', { COMMENTS: commentsHtml });
-  const res = new Response();
   res.setHeader('Content-Type', CONTENT_TYPES.html);
   res.setHeader('Content-Length', html.length);
   res.statusCode = 200;
-  res.body = html;
-  return res;
+  res.end(html);
 }
 
 const replaceUnknownChars = function(text, character) {
@@ -43,35 +40,49 @@ const replaceUnknownChars = function(text, character) {
   return text.replace(regEx, SYMBOLS[character]);
 };
 
-const saveCommentAndRedirect = function(req) {
+const pickupParams = (query, keyValue) => {
+  const [key, value] = keyValue.split('=');
+  query[key] = value;
+  return query;
+};
+
+const readParams = function(keyValueTextPairs) {
+  return keyValueTextPairs.split('&').reduce(pickupParams, {})
+};
+
+const saveCommentAndRedirect = function(req, res) {
+  let data = '';
   const comments = loadComments();
-  const date = new Date().toGMTString();
-  const { name, comment } = req.body;
-  const keys = Object.keys(SYMBOLS);
-  const [nameText, commentText] = [name, comment].map(text => keys.reduce(replaceUnknownChars, text));
-  comments.push({ date, name: nameText, comment: commentText });
-  if (!fs.existsSync('./data')) fs.mkdirSync('./data');
-  fs.writeFileSync('./data/comments.json', JSON.stringify(comments), 'utf8');
-  return serveGuestBookPage();
+  const date = new Date();
+  req.on('data', (chunk) => {
+    data += chunk;
+  });
+  req.on('end', () => {
+    const { name, comment } = readParams(data);
+    const keys = Object.keys(SYMBOLS);
+    const [nameText, commentText] = [name, comment].map(text => keys.reduce(replaceUnknownChars, text));
+    comments.push({ date, name: nameText, comment: commentText });
+    if (!fs.existsSync('./data')) fs.mkdirSync('./data');
+    fs.writeFileSync('./data/comments.json', JSON.stringify(comments), 'utf8');
+    serveGuestBookPage(req, res);
+  })
 }
 
-const serveStaticFile = (req, optionalUrl) => {
+const serveStaticFile = (req, res, optionalUrl) => {
   const path = `${STATIC_FOLDER}${optionalUrl || req.url}`;
   const stat = fs.existsSync(path) && fs.statSync(path);
   if (!stat || !stat.isFile()) return new Response();
   const [, extension] = path.match(/.*\.(.*)$/) || [];
   const contentType = CONTENT_TYPES[extension];
   const content = fs.readFileSync(path);
-  const res = new Response();
   res.setHeader('Content-Type', contentType);
   res.setHeader('Content-Length', content.length);
   res.statusCode = 200;
-  res.body = content;
-  return res;
+  res.end(content);
 }
 
-const serveHomePage = function(req) {
-  return serveStaticFile(req, '/home.html');
+const serveHomePage = function(req, res) {
+  return serveStaticFile(req, res, '/home.html');
 };
 
 const findHandler = (req) => {
@@ -81,9 +92,9 @@ const findHandler = (req) => {
   if (req.method === 'GET') return serveStaticFile;
   return () => new Response();
 }
-const processRequest = (req) => {
+const processRequest = (req, res) => {
   const handler = findHandler(req);
-  return handler(req);
+  handler(req, res);
 }
 
 module.exports = { processRequest };
